aeac2612616cd228e5406f7ddd9d01eee4165d96
Psaidwid <14smalleybra@gmail.com>
2019-04-15 15:16:06 -0400

    removed voting system for re-write and added new player names

---
+++ PollManager.js
@@ -0,0 +1,6 @@
+class Poll {
+  constructor(pName, pOptions, pVotes, pVoters) {
+    this.id = pID;
+		this.name = pName;
+	}
+}
--- app.js
+++ app.js
@@ -4,22 +4,7 @@ const settings = require('./settings.json');
 const prefix = '&';
 const admin = '155139566842413056';
 
-var dndPlayers;
-var votes = new Array();
-
-function sleep(milliseconds) {
-  var start = new Date().getTime();
-  for (var i = 0; i < 1e7; i++) {
-    if ((new Date().getTime() - start) > milliseconds){
-      break;
-    }
-  }
-}
-
-function getNickname()
-{
-
-}
+var dndPlayers = new Array();
 
 bot.on('ready',() => {
 	console.log('D&D Bot Online');
@@ -44,112 +29,63 @@ bot.on('message', message => {
 		else
 			messageString = ' rolled a ';
 
-		message.channel.send('<@' + message.author.id + '>' + messageString + dice);
+		message.channel.send(getCharacterName(message.author.id) + '(<@' + message.author.id + '>)' + messageString + dice);
 		console.log(message.author.username + messageString + dice);
 	}
 
-	if (message.content.startsWith(prefix + 'setname')) {
-		message.channel.send('Sorry this isn\'t ready yet');
+	if (message.content.startsWith(prefix + 'setname') && (message.content.split(" ").length > 1)) {
+    setCharacterName(message.author.id, message.content.split(" ")[1]);
+    console.log(message.author.username + ' set their character name to ' + message.content.split(" ")[1]);
+    message.channel.send('Hello ' + getCharacterName(message.author.id));
 	}
-
-	if (message.content.startsWith(prefix + 'sv') && message.content.split(" ") > 5) {
-
-		var messageContent = message.content.split(" ");
-		var newVote = new Vote(messageContent[1]);
-
-		newVote.voters = messageContent[2];
-
-		message.channel.send('A new vote \'' + newVote.name + '\' has been created for \'' + newVote.voters + '\' voters with the options\n')
-
-		for (var i = 3; i < messageContent.length; i++)
-		{
-			message.channel.send('```' + messageContent[i] + '```');
-			newVote.options.push(messageContent[i]);
-		}
-
-		for (var i = 3; i < messageContent.length; i++)
-			newVote.votes.push(0);
-
-
-		votes.push(newVote);
-		//console.log(votes);
-	}
-  else if (message.content.startsWith(prefix + 'sv')) {
-    message.channel.send('Sorry, too few arguments');
-    message.channel.send('You can start a vote by doing something like ```' + prefix + 'sv direction 3 north south east west```');
+  else if (message.content.startsWith(prefix + 'setname')) {
+    console.log(message.content.split(" "));
+    message.channel.send('Sorry, wrong arguments, use ```&setname bob``` to set your name to bob');
   }
 
-  if (message.content.startsWith(prefix + 'log')) {
-    console.log(votes);
-  }
-
-	if (message.content.startsWith(prefix + 'vote') && message.content.split(" ") > 3) {
-		var voteEnd = false;
-		var voteName = new String();
-    var vote = new Array();
-		for (var voteIndex = 0; voteIndex < votes.length; voteIndex++) {
-      if (votes[voteIndex].name = message.content.split(" ")[1]) {
-        vote = votes[voteIndex];
-      }
-		}
-
-    vote.votes[message.content.split(" ")[2] - 1] += 1;
-
-    var totalVotes = new Number();
-    var winner = vote.options[0];
-    var winnerVotes = new Number();
-
-    for (var i = 0; i < vote.votes.length; i++) {
-      totalVotes += vote.votes[i];
-      if (vote.votes[i] > winnerVotes)
-        winner = vote.options[i];
-    }
-
-    if (totalVotes == vote.voters)
-    {
-      voteEnd = true;
-      voteName = vote.name;
-      message.channel.send('The vote \'' + vote.name + '\' has ended and the winner is ' + winner);
-    }
-    else {
-      message.channel.send('The current stats are \n')
-      var stats = '```';
-      for (var i = 0; i < vote.options.length; i++)
-        stats += vote.options[i] + ': ' + vote.votes[i] + '\n';
-      stats += '```';
-      message.channel.send(stats);
-    }
-
-		if (voteEnd)
-		{
-			votes.splice(voteIndex, 1);
-		}
-
-		console.log(votes);
-	}
-  else if (message.content.startsWith(prefix + 'vote')) {
-    message.channel.send('Sorry, too few arguments');
-    message.channel.send('You can vote by doing something like ```' + prefix + 'vote direction 2```');
-  }
 });
 
 
 bot.login(settings.token);
 
-
-class Vote {
-	constructor(vName, vVoters)
-	{
-		this.name = vName;
-		this.options = new Array();
-		this.votes = new Array();
-		this.voters = new Number();
+class Player {
+	constructor(pID, pName) {
+    this.id = pID;
+		this.name = pName;
 	}
 }
 
-class Player {
-	constructor(pName, pID) {
-		this.name = pName;
-		this.id = pID;
-	}
+function sleep(milliseconds) {
+  var start = new Date().getTime();
+  for (var i = 0; i < 1e7; i++) {
+    if ((new Date().getTime() - start) > milliseconds){
+      break;
+    }
+  }
+}
+
+function getCharacterName(userID) {
+  charName = '';
+  for (i = 0; i < dndPlayers.length; i++)
+  {
+    if (dndPlayers[i].id == userID)
+    {
+      return dndPlayers[i].name;
+    }
+  }
+  return '';
+}
+
+function setCharacterName(userID, charName) {
+  for (i = 0; i < dndPlayers.length; i++)
+  {
+    if (dndPlayers[i].id == userID)
+    {
+      dndPlayers[i].name = charName;
+      return;
+    }
+  }
+
+  dndPlayers.push(new Player(userID, charName));
+  return;
 }
